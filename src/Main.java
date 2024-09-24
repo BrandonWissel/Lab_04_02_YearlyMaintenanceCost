a78a8593ae93cc53e8c004b4ba78772340a8fa10
bwiss <wisselbo@mail.uc.edu>
2024-09-23 21:53:50 -0400

    init

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double springMaintenanceCost = 868.27;
+        double summerMaintenanceCost = 2956.55;
+        double winterMaintenanceCost = 5054.50;
+        double fallMaintenanceCost = 1500.61;
+        double totalMaintenanceCost = summerMaintenanceCost + winterMaintenanceCost + fallMaintenanceCost + springMaintenanceCost;
+        System.out.println("Spring maintenance cost: " + springMaintenanceCost);
+        System.out.println("Summer maintenance cost: " + summerMaintenanceCost);
+        System.out.println("Winter maintenance cost: " + winterMaintenanceCost);
+        System.out.println("Fall maintenance cost: " + fallMaintenanceCost);
+        System.out.println("Total maintenance cost: " + totalMaintenanceCost);
+
+    }
+}
